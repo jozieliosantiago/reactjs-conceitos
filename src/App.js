da68e9910b3a4630d7ae46c3d4b38a1d1e06b3bf
Joziélio Santiago <jozieliosantiago@gmail.com>
2020-08-10 19:32:30 -0300

    list and add repositories

--- src/App.js
+++ src/App.js
@@ -1,29 +1,78 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
+import api from './services/api';
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState(null);
+  const [newRepository, setNewRepository] = useState('');
+
+  useEffect(() => {
+    async function fetchRepositories() {
+      try {
+        const response = await api.get('/repositories');
+        const { data } = response;
+
+        setRepositories(data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    fetchRepositories();
+  }, [])
+
   async function handleAddRepository() {
-    // TODO
+    const body = {
+      title: newRepository.trim(),
+    }
+
+    try {
+      const response = await api.post('/repositories', body);
+      const { data } = response;
+      console.log(repositories)
+
+      setRepositories((state) => [...state, data])
+
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    try {
+      await api.delete(`/repositories/${id}`);
+
+      const repositoriyList = repositories.filter((repositoriy) => repositoriy.id !== id);
+      setRepositories(repositoriyList);
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   return (
-    <div>
-      <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+    <div className="container">
+      <div className="content">
+        <h1>Lista de Repositórios</h1>
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
-      </ul>
+        <div className="search">
+          <input value={newRepository} onChange={(e) => setNewRepository(e.target.value)} />
+          <button onClick={handleAddRepository}>Adicionar</button>
+        </div>
 
-      <button onClick={handleAddRepository}>Adicionar</button>
+        <ul data-testid="repository-list">
+          {repositories && repositories.map((repository) => (
+            <li key={repository.id}>
+              <div>
+                <h4>{repository.title}</h4>
+              </div>
+              <button onClick={() => handleRemoveRepository(repository.id)}>
+                Remover
+            </button>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
